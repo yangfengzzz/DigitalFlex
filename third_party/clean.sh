2df21fc9a416ec4b43fd3da6b39a4ee35d229d2e
yangfengzzz <yangfengzzz@hotmail.com>
2022-07-21 22:00:48 +0800

    add third party

---
+++ third_party/clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+
+# ASSIMP #####################################
+cd assimp
+rm -rf build_release
+rm -rf build_debug
+cd ..
+
+# GoogleTest ##############################
+cd googletest
+rm -rf build
+cd ..
+
+set +x
